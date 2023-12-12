aba4beb3191ae08348ad45bbe77164c3a9185248
efecdml <efecdml@gmail.com>
2023-12-12 11:38:30 +0300

    Initial commit.

---
+++ src/main/java/com/gungorefe/simpleportfolio/SimplePortfolioApplication.java
@@ -0,0 +1,11 @@
+package com.gungorefe.simpleportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePortfolioApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePortfolioApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gungorefe/simpleportfolio/SimplePortfolioApplicationTests.java
@@ -0,0 +1,11 @@
+package com.gungorefe.simpleportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePortfolioApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
